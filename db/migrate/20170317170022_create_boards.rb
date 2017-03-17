f5fac754341b10dff8fcaa373998ad26ed1d61df
ppworks <koshikawa@ppworks.jp>
2017-03-18 02:02:42 +0900

    Tweak boards table definition

--- db/migrate/20170317170022_create_boards.rb
+++ db/migrate/20170317170022_create_boards.rb
@@ -1,7 +1,7 @@
 class CreateBoards < ActiveRecord::Migration[5.1]
   def change
     create_table :boards do |t|
-      t.string :key
+      t.string :key, null: false, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170317170022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_boards_on_key"
+  end
 
 end
