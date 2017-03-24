c5542e6f5c9606758ac6c2fcb1f1fb066ad06aea
ppworks <koshikawa@ppworks.jp>
2017-03-24 22:14:48 +0900

    Shorten URL

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,6 @@ import * as pages from './pages';
 document.addEventListener('turbolinks:load', () => {
   let router = new Router;
 
-  router.add('^/boards/', () => { new pages.CardPage() });
+  router.add('^/b/', () => { new pages.CardPage() });
   router.create();
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :kpt_boards, only: %i(create), path: 'kpt'
-  resources :boards, only: %i(show), param: :key do
+  resources :boards, only: %i(show), param: :key, path: 'b' do
     resources :categories, only: %i(), param: :key do
       resources :cards, only: %i(create update destroy), param: :key, shallow: true
     end
