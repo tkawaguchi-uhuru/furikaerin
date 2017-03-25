e95aba6f5adb62149eb1cd869ff4ee9c5ed93994
ppworks <koshikawa@ppworks.jp>
2017-03-25 12:19:52 +0900

    Bind all address for testing on local network

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,5 +55,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.x.webpacker[:dev_server_host] = 'http://localhost:8080'
+  config.x.webpacker[:dev_server_host] = ENV.fetch('WEBPACKER_DEV_SERVER_HOST', 'http://localhost:8080')
 end
