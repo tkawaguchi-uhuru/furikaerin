db0dd70e5a64e5a50ddb46c8a8c530483f5ca870
ppworks <koshikawa@ppworks.jp>
2017-03-25 16:44:58 +0900

    Reset connection when turbolinks:load

--- app/assets/javascripts/channels/board.js
+++ app/assets/javascripts/channels/board.js
@@ -1,14 +1,16 @@
-if (matches = location.pathname.match(new RegExp('^/b/([^\/\?/#]+)'))) {
-  App.cable.subscriptions.create({ channel: "BoardChannel", key: matches[1] }, {
-    connected: function() {
-    },
+document.addEventListener('turbolinks:load', function() {
+  if (matches = location.pathname.match(new RegExp('^/b/([^\/\?/#]+)'))) {
+    App.cable.subscriptions.create({ channel: "BoardChannel", key: matches[1] }, {
+      connected: function() {
+      },
 
-    disconnected: function() {
-    },
+      disconnected: function() {
+      },
 
-    received: function(data) {
-      Turbolinks.clearCache();
-      Turbolinks.visit(location.pathname, { action: 'replace' });
-    }
-  })
-}
+      received: function(data) {
+        Turbolinks.clearCache();
+        Turbolinks.visit(location.pathname, { action: 'replace' });
+      }
+    })
+  }
+});
